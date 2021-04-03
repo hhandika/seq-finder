577c753373bba43d6ef4e86f52fb37d201b3fe08
Heru Handika <herubiolog@gmail.com>
2021-04-03 13:15:12 -0500

    Print result to file.

--- src/finder.rs
+++ src/finder.rs
@@ -1,20 +1,45 @@
+use std::fs::File;
+use std::io::{LineWriter, Write};
+
 use regex::Regex;
 use walkdir::WalkDir;
 
-pub fn find_cleaned_fastq(path: &str, len: usize, sep: char) {
-    println!("[seq]");
+pub fn find_cleaned_fastq(path: &str, len: usize, sep: char, iscsv: bool) {
+    let output = File::create(get_fnames(iscsv)).expect("FILE EXISTS.");
+    let mut line = LineWriter::new(output);
+    if iscsv {
+        writeln!(line, "id,path").unwrap();
+    } else {
+        writeln!(line, "[seq]").unwrap();
+    }
     WalkDir::new(path)
         .into_iter()
         .filter_map(|ok| ok.ok())
         .filter(|e| e.file_type().is_file())
         .for_each(|e| {
-            let path = e.path().to_string_lossy();
+            let path = e.path().parent().unwrap();
             let fname = e.path().file_name().unwrap().to_string_lossy();
             if re_matches_lazy(&fname) {
                 let id = construct_id(&fname, len, sep);
-                println!("{}:{}", id, path);
+                let full_path = String::from(path.canonicalize().unwrap().to_string_lossy());
+                if iscsv {
+                    writeln!(line, "{},{}/", id, full_path).unwrap();
+                } else {
+                    writeln!(line, "{}:{}/", id, full_path).unwrap();
+                }
             }
         });
+}
+
+fn get_fnames(iscsv: bool) -> String {
+    let mut fname = String::from("seq-finder");
+    if iscsv {
+        fname.push_str(".csv");
+    } else {
+        fname.push_str(".conf");
+    }
+
+    fname
 }
 
 fn re_matches_lazy(fname: &str) -> bool {
